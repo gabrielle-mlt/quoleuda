0a05f1fad09392569a51fb1338409056538b3a62
картофелина <gabrielle.multan.pro@gmail.com>
2023-09-26 12:37:03 +0200

    chore: fix bug

--- src/pages/HangeulCheatSheet.vue
+++ src/pages/HangeulCheatSheet.vue
@@ -9,19 +9,25 @@
         <p class="font-italic">
           (Coming soon!)
         </p>
-
-        <v-btn
-          class="mt-4"
-          color="primary"
-          @click="$router.push({ name: 'QuizMenu' })"
-        >
-          I'm ready !
-        </v-btn>
       </v-col>
     </v-row>
+    <!--    <p>
+          At the origin, Hangeul was created following 2 principles:
+          <ul>
+            <li>
+              The <strong>shape</strong> of the letter is inspired by the shape of the mouth when pronouncing the
+              sound
+            </li>
+            <li>The <strong>sound</strong> of the letter is inspired by the sound of the letter</li>
+          </ul>
+        </p>-->
+
+    <v-btn
+      class="mt-4"
+      color="primary"
+      @click="$router.push({ name: 'QuizMenu' })"
+    >
+      I'm ready !
+    </v-btn>
   </v-container>
 </template>
-
-<style scoped>
-
-</style>
